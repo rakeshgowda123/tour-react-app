c49b778fa202e22f271db0c1d7763fd2d65b06a6
rakeshgowda123 <rakeshgowder251999@gmail.com>
2024-03-14 12:19:51 +0530

    chaged header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import React from "react";
 const Header=() =>{
     return(
         <div className="container">
-         <div className="title"> Plan a Tour</div> 
+         <div className="title"> Plan Your Journey with us!</div> 
         </div>
     )
 }
